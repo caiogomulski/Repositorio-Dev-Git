fc653623dc365b6c414b3e989298bcafeef7e88d
Gustavo <nebesgustavo@gmail.com>
2025-12-12 19:53:36 -0300

    feat: implementa botao de volta para o topo

--- main.js
+++ main.js
@@ -1518,6 +1518,19 @@ if (cartModal) {
     });
   }
 }
+const backToTopBtn = document.getElementById('backToTop');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.style.display = 'flex';
+  } else {
+    backToTopBtn.style.display = 'none';
+  }
+});
+
+backToTopBtn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
 
 updateSortButtonText();
 renderProducts(getVisibleProducts());
